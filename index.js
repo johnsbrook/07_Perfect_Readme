66bc7c5384ef092d2ebba67cd4d217924bccfed6
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-11 22:02:14 -0400

    created requirements

---
+++ index.js
@@ -0,0 +1,34 @@
+// ****************************
+// * ASK THESE QUESTIONS FIRST*
+// ****************************
+
+// Title of the Project
+// Description of the Project
+// Installation Instructions
+// Usage Information
+// Contribution Guidelines
+// Test Instructions
+
+// **************************************
+// * THEN, ADD ANSWERS TO THESE SECTIONS*
+// **************************************
+
+// Description 
+// Installation 
+// Usage 
+// Contributing 
+// Tests 
+
+// **********************************
+// *  GENERATED README MUST INCLUDE *
+// **********************************
+
+// Title 
+// Description 
+// Table of Contents 
+// Installation 
+// Usage 
+// License 
+// Contributing 
+// Tests 
+// Questions 
