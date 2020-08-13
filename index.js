01a117bedbc6e4cba0d09babb1495252dca2b167
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-12 20:16:14 -0400

    added license logo

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function promptUser() {
         },
 // Description of the Project
         {
-            type: "editor", 
+            type: "input", 
             message: "Type the description of your project. Note: Default editor will launch. Enter text, proceed to close editor and click on save.",
             name: "description"
         },
@@ -31,19 +31,19 @@ function promptUser() {
         },
 // // Usage Information
         {
-            type: "editor",
+            type: "input",
             message: "Enter your user story. Note: Default editor will launch. Enter text, proceed to close editor and click on save.",
             name: "usage"
         },
 // // Contribution Guidelines
         {
-            type: "editor",
+            type: "input",
             message: "Enter instructions on how other can contribute to your project.",
             name: "contribution"
         },
 // // Test Instructions
         {
-            type: "editor",
+            type: "input",
             message: "Enter instructions to test your code.",
             name: "test"
         },
@@ -51,7 +51,8 @@ function promptUser() {
             type: "list",
             message: "Select the license your project is covered under.",
             name: "license",
-            choices: ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", "BSD 2-clause 'Simplified' license", "BSD 3-clause Clear license", "Creative Commons license family", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Do What the F*ck You Want to Public License", "Educational Community License v2.0", "Eclipse Public License 1.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License family", "GNU General Public License v2.0", "GNU General Public License v3.0", "GNU Lesser General Public License family", "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project License v1.3c", "Microsoft Public License", "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "PostgreSQL Lincense", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"]
+            choices: ["Academic_Free_License_v3.0", "Apache_license_2.0", "Artistic_license_2.0", "Boost_Software_License_1.0", "BSD_2_clause_'Simplified'_license", "BSD 3_clause_Clear_license", "Creative_Commons_license_family", "Creative_Commons_Zero_v1.0_Universal", "Creative_Commons_Attribution_4.0", "Creative_Commons_Attribution_Share_Alike_4.0", "Do_What_the_F*ck_You_Want_to_Public_License", "Educational_Community_License_v2.0", "Eclipse_Public_License_1.0", "European_Union_Public_License_1.1", "GNU_Affero_General_Public_License_v3.0", "GNU_General_Public_License_family", "GNU_General_Public_License_v2.0", "GNU_General_Public_License_v3.0", "GNU_Lesser_General_Public_License_family", "GNU_Lesser_General_Public_License_v2.1", "GNU_Lesser_General_Public_License_v3.0", "ISC", "LaTeX_Project_License_v1.3c", "Microsoft_Public_License", "MIT", "Mozilla_Public_License_2.0", "Open_Software_License_3.0", "PostgreSQL_Lincense", "SIL_Open_Font_License_1.1", "University_of_Illinois/NCSA_Open_Source_License", "The_Unlicense", "zLib License"]
+            // choices: "MIT"
         }, 
         {
             type: "input",
@@ -79,23 +80,23 @@ ${response.description} \n
 
 ## Table of Contents \n
 
+* [Usage](#usage-information) \n
 * [Installation](#installation-instructions) \n
-* [Usage](#usage) \n
-* [License](#installation) \n
-* [Contributing](#constributing) \n
+* [License](#license) \n
+* [Where to Contribute](#where-to-contribute) \n
 * [Tests](#tests) \n
 * [Questions](#questions) \n
 
 ## Usage Information \n 
     ${response.usage} \n
 
-## Installation Instruction \n 
+## Installation Instructions \n 
     ${response.installation} \n
 
 ## License \n 
-${response.license} \n
+${response.license} [![License](https://img.shields.io/badge/License-${response.license}-brightgreen.svg)]  \n
     
-## Contributing \n 
+## Where to Contribute \n 
 ${response.contribution} \n
 
 ## Tests \n 
--- johnsbrook.md
+++ johnsbrook.md
@@ -1,40 +1,55 @@
 
-# Perfect README.md 
+# Project One 
 
 
 ## Description 
- This the description of my project:
-
-Create the perfect Readme.md file with this generator 
+ 
+The prurpose is to create the perfect README file generator. 
 
 
 ## Table of Contents 
 
 
-## Installation Instruction 
- 
-    npm install 
+* [Usage](#usage-information) 
+
+* [Installation](#installation-instructions) 
+
+* [License](#license) 
+
+* [Where to Contribute](#where-to=contribute) 
+
+* [Tests](#tests) 
+
+* [Questions](#questions) 
 
 
 ## Usage Information 
  
-    Create the perfect readme file 
+    This app is meant to create the perfect generator. 
+
+
+## Installation Instructions 
+ 
+    npm i 
 
 
 ## License 
- University of Illinois/NCSA Open Source License 
+ 
+Apache license 2.0 ![License]("https://img.shields.io/static/v1?label=License&message=Apache license 2.0 &color=green")
 
     
-## Contributing 
- These are the instructions 
+## Where to Contribute 
+ 
+You can contribute on the GitHub below.  
 
 
 ## Tests 
- These are the instructions to test my code.  
+ 
+To test this code, you're welcome to download to your computer and run using Node.js 
 
 
 ## Questions 
  
 GitHub: [johnsbrook](https://github.com/johnsbrook) 
  
-You may reach me by email at: [johnsbrook@email.com](mailto:johnsbrook@email.com); 
+You may reach me by email at: [johnsbrook@email.com](mailto:johnsbrook@email.com)
